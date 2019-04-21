0ac13ed750b028298a2838b3789a6baa17ba992c
Noiri <suleipenn@gmail.com>
2019-04-21 23:16:39 +0900

    solved A,B,C

---
+++ Tenka1-2019/A-On_the_Way.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+    int a, b, c;
+    cin >> a >> b >> c;
+    if((a <= c && c <= b) || (b <= c && c <= a)) cout << "Yes" << endl;
+    else cout << "No" << endl;
+    return 0;
+}
