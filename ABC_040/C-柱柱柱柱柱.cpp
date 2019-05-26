8beb20ba361c75eae01e3be1f23ae84b5caae30f
Noiri <suleipenn@gmail.com>
2019-05-27 02:23:15 +0900

    典型DP, edpc1系

--- ABC_040/C-柱柱柱柱柱.cpp
+++ ABC_040/C-柱柱柱柱柱.cpp
@@ -1,21 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int dp[155555];
+
 int main(){
     int n;
     cin >> n;
-    vector<int> v(n);
-    for(int i=0; i<n; i++) cin >> v[i];
+    vector<int> a(n);
+    for(int i=0; i<n; i++) cin >> a[i];
 
-    int ans = 0;
-    for(int i=0; i<n-1; i++){
-        if(i+2<n && abs(v[i+2] - v[i]) < abs(v[i+1] - v[i])){
-            ans += abs(v[i+2] - v[i]);
-            i++;
-        }
-        else {
-            ans += abs(v[i+1] - v[i]);
-        }
+    for(int i=1; i<=n; i++) dp[i] = 1e9;
+
+    for(int i=0; i<n; i++){
+        dp[i+1] = min(dp[i+1], dp[i]+ abs(a[i+1] - a[i]));
+        dp[i+2] = min(dp[i+2], dp[i] + abs(a[i+2] - a[i]));
     }
-    cout << ans << endl;
+
+    cout << dp[n-1] << endl;
+    return 0;
 }
