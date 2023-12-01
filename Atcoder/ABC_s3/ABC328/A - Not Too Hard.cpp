2103799619e10ade9f728cb55573eb6f8cf3a412
Noiri <suleipenn@gmail.com>
2023-12-02 06:35:50 +0900

    solved

---
+++ Atcoder/ABC_s3/ABC328/A - Not Too Hard.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using namespace std;
+using ll = long long;
+
+int main(){
+    int n, x;
+    cin >> n >> x;
+    vector<int> v(n);
+    rep(i, n) cin >> v[i];
+
+    int ans = 0;
+    rep(i, n){
+        if(v[i] <= x) ans += v[i];
+    }
+    cout << ans << endl;
+}
