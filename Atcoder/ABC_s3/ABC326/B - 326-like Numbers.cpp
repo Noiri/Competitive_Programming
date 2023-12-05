7559adbe95ca2dd455195097a64396879ae3c81e
Noiri <suleipenn@gmail.com>
2023-12-05 16:45:43 +0900

    solved

---
+++ Atcoder/ABC_s3/ABC326/B - 326-like Numbers.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using namespace std;
+using ll = long long;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = n; i < 1000; i++){
+        string s = to_string(i);
+        if((s[0]-'0') * (s[1]-'0') == (s[2]-'0')){
+            cout << i << endl;
+            return 0;
+        }
+    }
+}
