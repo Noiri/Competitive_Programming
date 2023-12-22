8eb0cdaf32965388a7aeab7a8f11f6c3a4bc13f0
Noiri <suleipenn@gmail.com>
2023-12-22 19:19:22 +0900

    solved

---
+++ Atcoder/ABC_s3/ABC289/A - flip.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using namespace std;
+using ll = long long;
+
+
+int main(){
+    string s;
+    cin >> s;
+    for(auto x: s){
+        if(x == '0') cout << "1";
+        else cout << "0";
+    }
+    cout << endl;
+}
