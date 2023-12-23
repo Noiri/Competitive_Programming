9e51ea2a0c0ce2b001e38c22c2e5138a15934a03
Noiri <suleipenn@gmail.com>
2023-12-24 04:10:09 +0900

    solved

---
+++ Atcoder/ABC_s3/ABC287/A - Majority.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using namespace std;
+using ll = long long;
+
+
+int main(){
+    int n;
+    cin >> n;
+    int cnt = 0;
+    rep(i, n){
+        string s;
+        cin >> s;
+        cnt += (s == "For");
+    }
+    if(cnt > n/2) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
