1199ace4e97c3ddf3765eccaacd02154a175276a
Noiri <suleipenn@gmail.com>
2024-01-02 21:32:12 +0900

    solved

---
+++ Atcoder/ABC_s3/ABC305/A - Water Station.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using namespace std;
+using ll = long long;
+
+
+int main(){
+    int n;
+    cin >> n;
+    int m = 1e9;
+    int ans = 0;
+    for(int i = 0; i <= 100; i += 5){
+        if(chmin(m, abs(n - i))) ans = i;
+    }
+
+    cout << ans << endl;
+}
