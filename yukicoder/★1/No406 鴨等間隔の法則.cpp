2607bf198da4e7b8f98d50f452d966a762db62cd
Noiri <suleipenn@gmail.com>
2024-01-28 13:29:09 +0900

    solved

---
+++ yukicoder/★1/No406 鴨等間隔の法則.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using namespace std;
+using ll = long long;
+
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> x(n);
+    for(int i = 0; i < n; i++) cin >> x[i];
+    sort(x.begin(), x.end());
+
+    bool ok = true;
+    int d = x[1] - x[0];
+    for(int i = 1; i < n; i++){
+        if(d == 0) ok = false;
+        else if(d != x[i] - x[i-1]) ok = false;
+    }
+
+    if(ok) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
