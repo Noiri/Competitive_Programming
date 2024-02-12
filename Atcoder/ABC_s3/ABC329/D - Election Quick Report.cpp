652334a8481bff49b22e0da5e33705d02a35a27d
Noiri <suleipenn@gmail.com>
2024-02-13 00:36:32 +0900

    solved

---
+++ Atcoder/ABC_s3/ABC329/D - Election Quick Report.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using namespace std;
+using ll = long long;
+
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    map<int, int> mp;
+    int max_val = 0;
+    int cand = 0;
+    rep(i, m){
+        int a;
+        cin >> a;
+        mp[a]++;
+        if(max_val < mp[a]){
+            cand = a;
+            max_val = mp[a];
+        }
+        if(max_val == mp[a]){
+            if(cand > a) cand = a;
+        }
+        cout << cand << endl;
+    }
+}
