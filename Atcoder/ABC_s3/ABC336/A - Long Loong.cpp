9262b76c9dcb6aca0bf0e15e46f7a237243e6c58
Noiri <suleipenn@gmail.com>
2024-04-22 23:17:14 +0900

    solve

---
+++ Atcoder/ABC_s3/ABC336/A - Long Loong.cpp
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using namespace std;
+using ll = long long;
+
+int main(){
+    int n;
+    cin >> n;
+
+    string ans = "L";
+    rep(i, n) ans += "o";
+    ans += "ng";
+
+    cout << ans << endl;
+}
