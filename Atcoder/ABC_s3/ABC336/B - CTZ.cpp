cc3c6b472c43961ce544993ed2bc4a0452e758e9
Noiri <suleipenn@gmail.com>
2024-04-23 18:51:41 +0900

    solved

---
+++ Atcoder/ABC_s3/ABC336/B - CTZ.cpp
@@ -0,0 +1,25 @@
+#include <assert.h>
+#include <bits/stdc++.h>
+
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define all(x) (x).begin(),(x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+#define UNIQUE(v) v.erase( unique(v.begin(), v.end()), v.end() );
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int ans = 0;
+    while(n > 0){
+        if(n % 2 == 0) ans++;
+        else break;
+        n /= 2;
+    }
+
+    cout << ans << endl;
+}
